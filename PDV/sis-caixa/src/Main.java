d395216ac4a211bd13d28e615ffc0f866548f5d8
ErisvaldoSantos-rgb <erisvaldo.cardoso@ba.estudante.senai.br>
2026-06-26 15:51:00 -0300

    Delete PDV/sis-caixa directory

--- PDV/sis-caixa/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        view.TelaLogin tela = new view.TelaLogin(); 
-    
-        tela.setVisible(true);
-    }
-}
